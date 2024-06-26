97719b4f62b188b80f7a2300ebf6dbd66e8a5e73
21SVeerepalli <159180879+21SVeerepalli@users.noreply.github.com>
2024-06-26 13:24:50 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Y9_DEC_TO_BIN_SKELETON
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
-
-            //MAIN:  NUMBER CONVERSION PROGRAM
-
-            //CODE GOES HERE
-        }
-
-        //static void means the function will not return a value so it does not need a data type 
-        //...this function DOES return a value so the method must have a data type
-        static string numberConversion(int number, int numberbase)
-        {
-            //CODE GOES HERE
-
-            return result; //REMOVE THE RED LINE!
-        }
-    }
-}
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+  
+namespace Y9_DEC_TO_BIN_SKELETON 
+{ 
+    internal class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode) 
+            int myInt = 0; //just for testing single stepping 
+            string myString = "12"; //watch me being cast from string to int 
+            string modMyString = myString.Insert(0, "My favourite number is "); 
+            Console.WriteLine(modMyString); 
+            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int 
+            Console.Write(myString); 
+  
+  
+             
+            //MAIN:  NUMBER CONVERSION PROGRAM 
+  
+            //CODE GOES HERE 
+         } 
+  
+        //static void means the function will not return a value so it does not need a data type  
+        //...this function DOES return a value so the method must have a data type 
+     //static string numberConversion(int number, int numberbase) 
+     //  { 
+     //       int calc = number + numberbase; 
+     //       string result = calc.ToString(); 
+  
+     //       return result; //REMOVE THE RED LINE! 
+     // } 
+    } 
+} 
+has context menu
